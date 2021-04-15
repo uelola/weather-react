847b2bbb951e0e674baf62c1ce2a2c19336f096f
uelola <80069286+uelola@users.noreply.github.com>
2021-04-15 17:48:40 +0200

    changed loader to colour violet

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
   return (
     <Loader
       type="Puff"
-      color="#00BFFF"
+      color="violet"
       height={100}
       width={100}
       timeout={3000} //3 secs
